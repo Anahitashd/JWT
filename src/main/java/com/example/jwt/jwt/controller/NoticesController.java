9cea583582ac96b499f6b71f5be5f0c14295d2de
Anahitashd <anahita_s76@yahoo.com>
2025-04-21 17:07:16 +0330

    add classes

--- src/main/java/com/example/jwt/jwt/controller/NoticesController.java
+++ src/main/java/com/example/jwt/jwt/controller/NoticesController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class NoticesController {
-
     @GetMapping("/notices")
     public String getNotices() {
         return "Here are the notice details from the DB";
