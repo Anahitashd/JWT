999045592932162174dfc7221b06676a4e64e60b
Anahitashd <anahita_s76@yahoo.com>
2025-04-22 11:00:50 +0330

    config security

--- src/main/java/com/example/jwt/jwt/EazyBackendApplication.java
+++ src/main/java/com/example/jwt/jwt/EazyBackendApplication.java
@@ -1,8 +1,6 @@
 package com.example.jwt.jwt;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.stereotype.Component;
 
 
 @SpringBootApplication
