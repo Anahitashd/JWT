1ba382a9f9805ea3f5413e5a61bd467aa73dea17
Anahitashd <anahita_s76@yahoo.com>
2025-04-27 13:04:22 +0330

    add database config and entity

--- src/main/java/com/example/jwt/jwt/EazyBackendApplication.java
+++ src/main/java/com/example/jwt/jwt/EazyBackendApplication.java
@@ -1,10 +1,14 @@
 package com.example.jwt.jwt;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
 @SpringBootApplication
 //@ComponentScan("com.example.jwt.jwt.controller")
+//@EnableJpaRepositories("com.example.jwt.jwt.repository")
+//@EntityScan("com.example.jwt.jwt.model")
 public class EazyBackendApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/jwt/jwt/config/ProjectSecurityConfig.java
+++ src/main/java/com/example/jwt/jwt/config/ProjectSecurityConfig.java
@@ -2,19 +2,17 @@ package com.example.jwt.jwt.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import org.springframework.security.authentication.password.CompromisedPasswordChecker;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class ProjectSecurityConfig {
@@ -34,14 +32,15 @@ public class ProjectSecurityConfig {
     }
 
     @Bean
-    public UserDetailsService userDetailsService(){
-        UserDetails user= User.withUsername("user")
-                .password("{noop}AnahitaShd@12345")
-                .authorities("read").build();
-        UserDetails admin= User.withUsername("admin")
-                .password("{bcrypt}$2a$12$gCbKELf3hdvIo6CeKR8JZ.xwXXfgn4gig1y8eT9SgCGL0bpN6zw4m")
-                .authorities("admin").build();
-        return new InMemoryUserDetailsManager(user,admin);
+    public UserDetailsService userDetailsService(DataSource dataSource){
+//        For saving in memory
+//        UserDetails user= User.withUsername("user")
+//                .password("{noop}AnahitaShd@12345")
+//                .authorities("read").build();
+//        UserDetails admin= User.withUsername("admin")
+//                .password("{bcrypt}$2a$12$gCbKELf3hdvIo6CeKR8JZ.xwXXfgn4gig1y8eT9SgCGL0bpN6zw4m")
+//                .authorities("admin").build();
+        return new JdbcUserDetailsManager(dataSource);
     }
 
     @Bean
